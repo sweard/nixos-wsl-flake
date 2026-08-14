d2c6782fd7f86fef4a723aec11650486dafc6e28
fangjian <fangjian@wondertechlabs.com>
2026-08-14 15:42:46 +0800

    feat(vmware): enable OpenSSH access

--- hosts/vmware/default.nix
+++ hosts/vmware/default.nix
@@ -4,4 +4,14 @@
     ../../modules/nixos/native-workstation.nix
     ./hardware.nix
   ];
+
+  # 允许通过 VMware NAT 网络从宿主机维护虚拟机。
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
 }
